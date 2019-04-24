5306dbb5cb91a6ebc308bb485063ac6c1edec03c
Dani Domínguez <dani.d.zamorano@gmail.com>
2019-04-24 19:46:34 +0200

    add route to update item

--- routes/api/items.js
+++ routes/api/items.js
@@ -28,13 +28,30 @@ router.post('/', (req, res) => {
 	newItem.save().then((item) => res.json(item));
 });
 
+// @route   PUT api/items/:id
+router.put('/:id', (req, res) => {
+	Item.findById(req.params.id)
+		.then((item) => {
+			let updatedItem = {};
+
+			if (req.body.name) updatedItem.name = req.body.name;
+			if (req.body.description) updatedItem.description = req.body.description;
+			if (req.body.price) updatedItem.price = parseFloat(req.body.price);
+
+			Item.findOneAndUpdate({ _id: req.params.id }, { $set: updatedItem }, { new: true }).then((item) =>
+				res.json(item)
+			);
+		})
+		.catch((err) => res.status(404).json({ itemnotfound: 'Item no encontrado' }));
+});
+
 // @route   DELETE api/items/:id
 router.delete('/:id', (req, res) => {
 	Item.findById(req.params.id)
 		.then((item) => {
 			item.remove().then(() => res.json({ success: 'true' }));
 		})
-		.catch((err) => res.status(404).json({ itemnofound: 'Item no encontrado' }));
+		.catch((err) => res.status(404).json({ itemnotfound: 'Item no encontrado' }));
 });
 
 module.exports = router;
