8437d89e9ec6a7a59d1d5428d557dc8c2de20898
Dani Domínguez <dani.d.zamorano@gmail.com>
2019-04-24 19:00:42 +0200

    add delete item route

--- routes/api/items.js
+++ routes/api/items.js
@@ -28,4 +28,13 @@ router.post('/', (req, res) => {
 	newItem.save().then((item) => res.json(item));
 });
 
+// @route   DELETE api/items/:id
+router.delete('/:id', (req, res) => {
+	Item.findById(req.params.id)
+		.then((item) => {
+			item.remove().then(() => res.json({ success: 'true' }));
+		})
+		.catch((err) => res.status(404).json({ itemnofound: 'Item no encontrado' }));
+});
+
 module.exports = router;
