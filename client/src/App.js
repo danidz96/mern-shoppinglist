69954d67d8b34bf30c25c1bce7ac72a1846f90db
Dani Domínguez <dani.d.zamorano@gmail.com>
2019-04-24 20:37:56 +0200

    create dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
+import Dashboard from './components/Dashboard/Dashboard';
 import './styles/App.css';
 
 const App = () => {
@@ -9,7 +10,7 @@ const App = () => {
 		<Router>
 			<div className="App">
 				<Navbar />
-
+				<Dashboard />
 				<Footer />
 			</div>
 		</Router>
