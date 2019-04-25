5bcbd8da8833819b8c5a895646f04b098b26af30
Dani Domínguez <dani.d.zamorano@gmail.com>
2019-04-25 18:04:13 +0200

    add create item component

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 import Dashboard from './components/Dashboard/Dashboard';
+import CreateItem from './components/CreateItem/CreateItem';
 import './styles/App.css';
 
 const App = () => {
@@ -10,7 +11,8 @@ const App = () => {
 		<Router>
 			<div className="App">
 				<Navbar />
-				<Dashboard />
+				<Route exact path={'/'} component={Dashboard} />
+				<Route exact path={'/create-item'} component={CreateItem} />
 				<Footer />
 			</div>
 		</Router>
