089a02cd2ebcd552dd4d66ecab3dc456db3a2546
Dani Domínguez <dani.d.zamorano@gmail.com>
2019-04-25 17:39:44 +0200

    show item list

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import ItemCard from '../ItemCard/ItemCard';
+import axios from 'axios';
 
 const Dashboard = () => {
+	const [ items, setItems ] = useState([]);
+
+	useEffect(
+		() => {
+			const fetchData = async () => {
+				const result = await axios.get('/api/items');
+				setItems(result.data);
+			};
+			fetchData();
+		},
+		[ items.length ]
+	);
+
 	return (
 		<div className="container">
 			<div className="create-item mb-5">
@@ -10,7 +24,17 @@ const Dashboard = () => {
 					Crear nuevo item
 				</Link>
 			</div>
-			<ItemCard />
+			<ul className="row">
+				{items.length > 0 ? (
+					items.map((item) => (
+						<div class="mb-2 col-lg-4 col-md-6 col-xs-12">
+							<ItemCard item={item} key={item._id} />
+						</div>
+					))
+				) : (
+					'Cargando...'
+				)}
+			</ul>
 		</div>
 	);
 };
--- client/src/components/ItemCard/ItemCard.js
+++ client/src/components/ItemCard/ItemCard.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import './styles.css';
 
-const ItemCard = () => {
+const ItemCard = ({ item }) => {
 	return (
-		<div className="card card-item">
-			<div className="card-body">
-				<h5 className="card-title">Card title</h5>
-				<p className="card-text">
-					Some quick example text to build on the card title and make up the bulk of the card's content.
-				</p>
-				<a className="card-link">Card link</a>
-				<a className="card-link">Another link</a>
+		<div className="col-sm-3">
+			<div className="card card-item">
+				<div className="card-body">
+					<h5 className="card-title">{item.name}</h5>
+					<h6 className="card-subtitle mb-2 text-muted">{item.price} €</h6>
+					<p className="card-text">{item.description}</p>
+					<div className="button-container">
+						<button className="btn add-to-cart btn-success btn-sm">
+							<i className="fas fa-shopping-cart" />
+						</button>
+						<button className="btn btn-action btn-secondary btn-sm">
+							<i className="far fa-edit" />
+						</button>
+						<button className="btn btn-action btn-danger btn-sm">
+							<i className="far fa-trash-alt" />
+						</button>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
