c2d1c78285057f41dc28c468f6acba5c6b409bec
Dani Domínguez <dani.d.zamorano@gmail.com>
2019-04-27 13:44:00 +0200

    fix error

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use('/api/items', items);
 app.use('/api/cart', cart);
 
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('client/static'));
+	app.use(express.static('client/build'));
 
 	app.get('*', (req, res) => {
 		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
