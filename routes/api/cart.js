8e8fa1ea014ac0af0ca084c136b6cb0d89f644a1
Dani Domínguez <dani.d.zamorano@gmail.com>
2019-04-27 11:17:10 +0200

    add delete item from cart route

--- routes/api/cart.js
+++ routes/api/cart.js
@@ -31,4 +31,19 @@ router.post('/:item_id', (req, res) => {
 		.catch((err) => res.status(404).json({ noitem: 'No se ha encontrado el item' }));
 });
 
+// @route   DELETE api/cart/:item_id
+router.delete('/:item_id', (req, res) => {
+	Item.findById(req.params.item_id)
+		.then((item) => {
+			Cart.findOne({}, {}, { sort: { created_at: -1 } })
+				.then((cart) => {
+					// TODO: mirar que no elimine todas las referencias
+					cart.items = cart.items.filter((itemCart) => (itemCart != req.params.item_id));
+					cart.save().then((cart) => res.json(cart));
+				})
+				.catch((err) => console.log(err));
+		})
+		.catch((err) => res.status(404).json({ noitem: 'No se ha encontrado el item' }));
+});
+
 module.exports = router;
